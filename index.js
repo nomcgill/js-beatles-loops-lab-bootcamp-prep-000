8ee58e2f4ccc6911626bb2b1ae40614959dd5599
Nolan McGill <nomcgill@gmail.com>
2018-09-09 23:51:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
- var array = []
+ var decision = []
  var who = musicians.length
  
   for (let who = 0; who < 4; who++)
   {console.log(musicians[who] + "plays" + instruments[who])
   }
-return 
-  
-}
+return decision
+  }
