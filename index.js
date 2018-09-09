172d6a074311153bb98b8e7bdc0ff8cb93717b20
Nolan McGill <nomcgill@gmail.com>
2018-09-09 23:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function theBeatlesPlay(musicians,instruments){
+function theBeatlesPlay(musicians, instruments){
+ musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+ instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+ var plays = " plays ";
 
-  
+  for (let i = 0; i < 4; i++)
+  { 
+    return musicians[i] + plays + instruments[i];
+  }
 }
